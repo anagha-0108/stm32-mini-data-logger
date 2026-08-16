0289af1a483de14f1626f0b3276fcc8926a7a1fa
Anagha <anaghamenath@gmail.com>
2026-08-16 17:37:42 +0530

    STM32 mini data logger: ADC temp sensor, moving average filter, PWM, UART logging, button pause/resume

---
+++ Core/Src/main.c
@@ -0,0 +1,483 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include<stdio.h>
+#include<string.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
+TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim3;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+#define WINDOW_SIZE 8
+
+volatile uint16_t adc_buffer[WINDOW_SIZE];
+volatile uint8_t  buffer_index            = 0;
+volatile uint8_t  buffer_full              = 0;   // becomes 1 once we've wrapped around once
+volatile uint8_t  new_sample_ready        = 0;
+volatile uint8_t  paused                  = 0;           // 0 = running, 1 = paused
+volatile uint32_t last_press_time         = 0;  // remembers when we last accepted a press
+#define DEBOUNCE_MS 200
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_ADC1_Init(void);
+static void MX_TIM2_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_TIM3_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_ADC1_Init();
+  MX_TIM2_Init();
+  MX_USART2_UART_Init();
+  MX_TIM3_Init();
+  /* USER CODE BEGIN 2 */
+  ADC->CCR |= ADC_CCR_TSVREFE;     // to power up the temperature sensor from power down mode
+  HAL_TIM_Base_Start(&htim2);      // start TIM2 counting -> generates periodic TRGO
+  HAL_ADC_Start_IT(&hadc1);        // arm ADC to convert on next trigger, IRQ on completion
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1); // start PWM output on TIM3 CH1
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	  if (new_sample_ready)
+	      {
+	          new_sample_ready = 0;
+
+	          if (!paused)      // if PC13 pressed not pressed
+	          {
+	              uint32_t sum = 0;
+	              uint8_t  count = buffer_full ? WINDOW_SIZE : buffer_index;
+
+	              for (uint8_t i = 0; i < count; i++)
+	              {
+	                  sum += adc_buffer[i];
+	              }
+
+	              uint16_t filtered_value = sum / count;
+
+	              // PWM DUTY CYCLE
+	              uint16_t ccr_value = ((uint32_t)filtered_value * 999) / 4095; // to compute the capture compare register value
+	              __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, ccr_value);
+	              uint16_t pwm_percentage =((uint32_t) ccr_value *100)/ (999+1); // d% = CCR*100 /(ARR+1);
+
+	              // UART TRANSMISSION
+	              char uart_msg[64];
+	              sprintf(uart_msg, "Filtered: %u,CCR:%u, PWM: %u%%\r\n", filtered_value, ccr_value,pwm_percentage );
+	              HAL_UART_Transmit(&huart2, (uint8_t*)uart_msg, strlen(uart_msg), HAL_MAX_DELAY);
+	          }
+	      }
+
+
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_RISING;
+  hadc1.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T2_TRGO;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_480CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 8399;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 999;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 83;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 999;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+  HAL_TIM_MspPostInit(&htim3);
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)
+{
+    uint16_t sample = HAL_ADC_GetValue(&hadc1);   // read the converted 12-bit result out of ADC1's data register
+
+    adc_buffer[buffer_index] = sample;            // store it at the current circular-buffer slot
+
+    buffer_index++;                                // advance to next slot
+    if (buffer_index >= WINDOW_SIZE)
+    {
+        buffer_index = 0;                          // wrap around
+        buffer_full = 1;                         // we've now completed at least one full lap
+    }
+
+    new_sample_ready = 1;							// tell main new data available
+}
+
+
+void  HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+	if(GPIO_Pin == GPIO_PIN_13){ // external interrupt
+		uint32_t current_press_time = HAL_GetTick();
+		if ((current_press_time - last_press_time) > DEBOUNCE_MS)
+		        {
+		            paused = !paused;
+		            last_press_time = current_press_time;
+		        }
+
+	}
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ README_1.md
@@ -0,0 +1,70 @@
+# STM32 Mini Data Logger — Xalten Systems Take-Home
+
+## Overview
+ADC1 samples the internal temperature sensor every 100ms, triggered by TIM2 (no
+`HAL_Delay()` polling). Each sample feeds an 8-point circular-buffer moving-average
+filter. The filtered value drives PWM on TIM3 (~1kHz), and the filtered value, CCR,
+and duty percentage stream over USART2 (115200 baud) each sample. The Nucleo user
+button (PC13, EXTI) pauses/resumes the loop with software debounce.
+
+No hardware was available for this submission. Builds clean in CubeIDE 2.1.1,
+0 errors / 0 warnings.
+
+## Design decisions
+
+**Analog input:** Used the internal temp sensor (`ADC_CHANNEL_TEMPSENSOR`) since I
+had no external pot to wire up. Per RM0383 and the datasheet, it needs at least
+10µs of sampling time — the closest available step in CubeMX is 480 cycles, so
+that's what I used.
+
+**Interrupt vs DMA:** Used TIM2->ADC via TRGO with `HAL_ADC_ConvCpltCallback()`
+rather than DMA. I've worked with interrupts and timers before, and while DMA cuts
+down CPU load, it's really meant for high-rate conversions — a single reading
+every 100ms doesn't call for it, so interrupts felt like the right tradeoff here.
+
+**ISR-to-main handoff:** The ADC callback just reads the value, stores it in a
+circular buffer, advances the index (wrapping back to 0 and setting `buffer_full`
+once it's gone all the way around), and sets a `volatile new_sample_ready` flag —
+no math or UART calls inside the ISR itself. The main loop does the actual
+averaging/PWM/UART work once that flag is set, and only averages over
+`buffer_index` samples until the buffer has filled once, so it's not averaging in
+garbage/uninitialized slots early on.
+
+**Debounce:** Timestamp-based, using `HAL_GetTick()` — similar in spirit to
+Arduino's `millis()`, which I'd used before. The EXTI callback only toggles the
+`paused` flag if more than 200ms has passed since the last accepted press. B1 is
+active-low with an external pull-up on the board, so it's configured
+`GPIO_MODE_IT_FALLING` with no internal pull needed.
+
+**Clock/timer math:** SYSCLK is 84MHz. APB1's prescaler is /2, but APB1 timer
+clocks get automatically doubled by hardware whenever that prescaler isn't 1, so
+TIM2 and TIM3 both actually run at 84MHz. TIM2 uses Prescaler=8399, Period=999 for
+a 10Hz sample trigger (TRGO on Update Event). TIM3 uses Prescaler=83, Period=999
+for a 1kHz PWM.
+
+**PWM output:** The filtered ADC value (0–4095) gets scaled down to the timer's
+0–999 count range and written straight into the CCR register with
+`__HAL_TIM_SET_COMPARE()` — that's what actually drives PA6. The duty cycle
+percentage sent over UART is a separate calculation, `D% = CCR/(ARR+1) × 100`,
+just for readability in the log.
+
+## What I learned from scratch
+- **CubeMX/HAL toolchain:** HAL's weak-callback override pattern for
+  `HAL_ADC_ConvCpltCallback()` and `HAL_GPIO_EXTI_Callback()`; the
+  `USER CODE BEGIN/END` marker system (I lost code once by placing it outside a
+  protected marker, and the next regeneration wiped it); and the automatic
+  doubling of APB1 timer clocks relative to APB1 peripheral clocks, which I had
+  to account for to get correct Prescaler/Period values.
+- **Internal temperature sensor:** it has to be manually roused via the TSVREFE
+  bit in `ADC->CCR`, and needs a minimum 10µs ADC sampling time, which forced me
+  to use the highest available SamplingTime step.
+- **Circular buffer / moving average:** how a fixed-size window absorbs a new
+  value by overwriting the oldest one once it's full, and once the index reaches
+  the window size it just resets back to index 0, and averages over those 8
+  values with the new value along with the 7 old ones.
+
+## References
+- "Hands-On with STM32 Timers: Trigger Periodic ADC Conversions" —
+  https://www.youtube.com/watch?v=Yt5cHkmtqlA
+- "STM32 Guide #3: PWM + Timers" — https://www.youtube.com/watch?v=AjN58ceQaF4
+- STM32F411 Reference Manual (RM0383) and Datasheet
